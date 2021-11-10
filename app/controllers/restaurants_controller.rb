9755baea0878844c75f8f5a4344d180d92a193c4
sophie callault <s.callault@gmail.com>
2021-11-10 22:07:06 +0100

    exo a

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,5 +6,26 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  ressources :restaurants
-  #   collection do
-  #     get
-    end
+  resources :restaurants
 end
